d751fa833a60351bec928dde4f313df3d4e65c70
Paulo <prsonda@gmail.com>
2023-03-08 15:22:00 -0300

    ajustes de erros de escrita e ad novas tecnologias

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,29 +2,29 @@ import styles from "@/styles/About.module.css";
 import Image from "next/image";
 
 export default function Aside() {
-	return (
-		<>
-			<aside className={styles.about}>
-				<section>
-					<Image
-						className={styles.imagemAlternativa}
-						src="/Paulo.png"
-						alt="Foto Paulo"
-						width={240}
-						height={280}
-					/>
-				</section>
-				<br />
+  return (
+    <>
+      <aside className={styles.about}>
+        <section>
+          <Image
+            className={styles.imagemAlternativa}
+            src="/Paulo.png"
+            alt="Foto Paulo"
+            width={240}
+            height={280}
+          />
+        </section>
+        <br />
 
-				<article>
-					<h1>Paulo Robson da Silva</h1>
-					<br />
-					<p>
-						Desenvolvimento de Software Back-end Nodejs | Javascript |
-						PostgreSQL | MySQL | API REST | Expressar
-					</p>
-				</article>
-			</aside>
-		</>
-	);
+        <article>
+          <h1>Paulo Robson da Silva</h1>
+          <br />
+          <p>
+            Desenvolvimento de Software Backend | API REST | Nodejs | Javascript
+            | Typescript | PostgreSQL | MySQL | Express | Knexjs
+          </p>
+        </article>
+      </aside>
+    </>
+  );
 }
